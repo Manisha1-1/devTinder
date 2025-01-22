c3bf7cd707e9dbe9eb74fe9204de1ee7534d401f
manisha <manisharawat209@gmail.com>
2025-01-22 23:03:31 +0530

    exploring routing

--- src/app.js
+++ src/app.js
@@ -14,9 +14,15 @@ app.use("/api/2",(req,res) =>{
 });
 
 
-app.use("/users",(req,res) =>{  
+app.use("/user?s",(req,res) =>{  
   res.send('hiiiii hello');
 });
+
+
+app.get(/a/,(req,res) =>{  
+  console.log(req.params);
+  res.send('Hello World two 1234');
+});//http://localhost:3000/car valid route it works it will match all the routes which has a in it
 
 app.get("/users",(req,res)=>{
   res.send({username:"manisha",password:"qwerty"});
@@ -32,9 +38,27 @@ app.use("/api",(req,res) =>{
   res.send('Hello World two');
 });
 
-app.use("/",(req,res) =>{  
-  res.send('Hello World');
+
+app.use("/api1/:userId/:name/:password",(req,res) =>{  
+  console.log(req.params);
+  res.send('Hello World two');
 });
+
+
+app.use("/a(pi)+*1",(req,res) =>{  
+  console.log(req.params);
+  res.send('Hello World two 12');
+});/// http://localhost:3000/apipipi1111111 valid route it works
+
+
+app.get(/a/,(req,res) =>{  
+  console.log(req.params);
+  res.send('Hello World two 123');
+});/// http://localhost:3000/apipipi1111111 valid route it works
+
+// app.use("/",(req,res) =>{  
+//   res.send('Hello World');
+// });
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
